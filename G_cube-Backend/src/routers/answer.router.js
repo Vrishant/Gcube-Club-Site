e1ac196adcbcd38c44907d317ae01b534538bcfe
MrAstatine <akshatkrish2005@gmail.com>
2025-02-17 15:22:36 +0530

    add auth middleware

--- G_cube-Backend/src/routers/answer.router.js
+++ G_cube-Backend/src/routers/answer.router.js
@@ -1,7 +1,8 @@
 import {registerAnswer} from '../controllers/answer.controller.js';
 import {Router} from 'express';
+import {getUserBeforeInput} from '../middlewares/auth.middleware.js';
 const router = Router()
 
-router.route('/register').post(registerAnswer)
+router.route('/register').post(getUserBeforeInput,registerAnswer)
 
 export default router;
--- G_cube-Backend/src/routers/user.router.js
+++ G_cube-Backend/src/routers/user.router.js
@@ -1,10 +1,11 @@
 import { registerUser , getUser , userAnswers } from "../controllers/user.controller.js";
 import {Router} from 'express';
+import { getUserBeforeInput } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route('/register').post(registerUser)
-router.route('/profile').get(getUser)
-router.route('/answers').get(userAnswers)
+router.route('/profile').get(getUserBeforeInput,getUser)
+router.route('/answers').get(getUserBeforeInput,userAnswers)
 
 export default router;
